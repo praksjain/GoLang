118b4671b19723ee8b896a9379d18e6ff5d9fb23
prakharjain <prakhar.jain@lntinfotech.com>
2020-02-22 15:55:14 +0530

    chg:basic_understanding|executable file

---
+++ basic_understanding.go
@@ -0,0 +1,57 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/* Module for BASICS OF GO . */
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/*Copyright © Prakhar Jain.
+Author : praks.jain7@gmail.com
+Version : 1.0
+Description : Script to understand basics of go (adding, concatenating, pointers).
+File Name : basic_understanding.go
+*/
+
+// Main
+package main
+
+// Built -in imports
+import "fmt"
+
+
+// adding two numbers and returning float value
+func add(x float64, y float64) float64{
+	return x+y
+}
+
+// concatenating two strings
+func concat(x string, y string) (string, string) {
+	return x,y
+}
+
+// reassigning the values
+func reassignment() float64 {
+	var num1 int = 61
+	var num2 float64 = float64(num1)
+	return num2
+}
+
+// use of pointers
+func point () {
+	x := 5
+	a := &x
+	fmt.Println("memory address: ", a)
+	fmt.Println("Value:", *a)
+	fmt.Println("New address of a:", &a)
+	fmt.Println("New value:", *&a)
+	fmt.Println("New value:", **&a)
+}
+
+// main method to call operations
+func main() {
+	fmt.Println("welcome go..!!")
+	num1, num2 := 3.2, 3.2
+	fmt.Println("Sum of two numbers is:", add(num1, num2))
+	a1, a2 := "Prakhar", "Jain"
+	fmt.Println(concat(a1,a2))
+	fmt.Println(reassignment())
+	point()
+}
